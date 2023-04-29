449daff1cce3b956bc77658a78ca846a5121bd33
Alex Kireeff <alexkireeff@gmail.com>
2023-04-28 20:47:21 -0400

    * added working parser

---
+++ Parser.hs
@@ -0,0 +1,22 @@
+import Types
+
+
+
+is_int :: String -> Bool
+is_int [] = True
+is_int (x : xs) = and [elem x "1234567890", is_int xs]
+
+
+parse_wrapper :: String -> [Expression]
+parse_wrapper input = parse (reverse $ words input) []
+
+parse :: [String] -> [Expression] -> [Expression]
+parse [] expr = expr
+parse ("+" : rest) (x:y:xs) = parse rest ((Function "+" [x, y]) : xs)
+parse ("-" : rest) (x:y:xs) = parse rest ((Function "-" [x, y]) : xs)
+parse ("i" : rest) xs = parse rest (Imaginary : xs)
+parse (pat : rest) xs =
+    if (is_int pat)
+    then parse rest (Integer (read pat :: Int) : xs)
+    else parse rest (Variable pat : xs)
+
