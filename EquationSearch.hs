2590e80cc2f9fe233007fbef7989956f52448708
Alex Kireeff <alexkireeff@gmail.com>
2023-05-12 04:12:39 -0400

    * modify_and_keep_best is now multithreaded

--- EquationSearch.hs
+++ EquationSearch.hs
@@ -1,38 +1,43 @@
 module EquationSearch(modify_and_keep_best) where
 
 import CAS
+import Control.Concurrent (forkIO, killThread, threadDelay)
 import Control.Concurrent.STM (TVar, atomically, readTVar, writeTVar)
 import Control.Monad (replicateM)
 import System.Random (randomRIO)
 import Types
 
 
-iterations_remaining = 1000000
-reset_every_n_iterations = 100
+timeout_seconds :: Int = 10
+threads :: Integer = 10
+reset_prob_percent :: Integer = 10
 penalty_function = equations_num_nodes
 
 modify_and_keep_best :: TVar [Equation] -> IO ()
 modify_and_keep_best tvar = do
   equations <- atomically $ readTVar tvar
   best_penalty <- atomically $ return $ penalty_function equations
-  iterate_modify equations best_penalty iterations_remaining
-  where
-    iterate_modify :: [Equation] -> Integer -> Integer -> IO ()
-    iterate_modify _ _ 0 = return ()
-    iterate_modify equations best_penalty iterations_remaining = do
+  tids <- mapM (\x -> forkIO (iterate_modify equations best_penalty)) [1..threads]
+  threadDelay $ timeout_seconds * (10 ^ 6)
+  _ <- mapM (killThread) tids
+  pure ()
+  where 
+    iterate_modify :: [Equation] -> Integer -> IO ()
+    iterate_modify equations best_penalty = do
       modified_equations <- modify_equations_randomly equations
       new_penalty <- atomically $ return $ penalty_function modified_equations
+      random_number <- randomRIO (1, 100)
       if (new_penalty < best_penalty)
         then do
           atomically $ writeTVar tvar modified_equations
-          iterate_modify modified_equations new_penalty (iterations_remaining - 1)
+          iterate_modify modified_equations new_penalty
         else
-          if (0 == rem iterations_remaining reset_every_n_iterations) -- restart at known best set of equations
-            then do
+          if (random_number <= reset_prob_percent)
+            then do -- restart at known best set of equations
               equations <- atomically $ readTVar tvar
-              iterate_modify equations best_penalty (iterations_remaining - 1)
+              iterate_modify equations best_penalty
             else do
-              iterate_modify modified_equations best_penalty (iterations_remaining - 1)
+              iterate_modify modified_equations best_penalty
 
 equations_num_nodes :: [Equation] -> Integer
 equations_num_nodes equations = sum $ map equation_num_nodes equations
